04823f1725dba71412e64465cf9de8954a28d34a
Reggie Dabu <prengay@yahoo.com>
2017-04-20 15:52:45 +0800

    final address fixed

--- application/views/partials/purchasing_receipt.php
+++ application/views/partials/purchasing_receipt.php
@@ -89,7 +89,8 @@
                 
                 <div class="invoice-box-total">
                     <b><i>Remarks:</i></b> {{purchase.header.Comments}}
-					<p style="margin-bottom:0">Expected Delivery on {{purchase.header.DeliveryDate | date:'MM/dd/yyyy'}} @ {{purchase.header.Description}}</p>
+					<p style="margin-bottom:0">Delivery Date {{purchase.header.DeliveryDate | date:'MM/dd/yyyy'}}</p>
+					<p style="margin-bottom:0">Delivery to {{purchase.header.Description}}</p>
 					<p>Address: {{purchase.header.Address}}</p>
                 </div>
 				
