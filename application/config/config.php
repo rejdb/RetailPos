23653c019d2a4846d38eedf71940759f47a4462f
Reggie Dabu <prengay@yahoo.com>
2017-04-27 09:23:41 +0800

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://tipos.angular.js:81';
-$config['base_url'] = 'https://techbox-dev.herokuapp.com';
+$config['base_url'] = '/';
+// $config['base_url'] = 'https://techbox-dev.herokuapp.com';
 // $config['base_url'] = 'http://tipos.angular.js';
 
 /*
